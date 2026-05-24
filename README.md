f9422facc572ba737ce83d00ae30aaa471b57711
Arseniy-Polyakov <arseney02@mail.ru>
2026-05-24 15:08:37 +0300

    chrf metric

--- README.md
+++ README.md
@@ -19,10 +19,10 @@ The functional method scheme is provided here:
 
 ### Experimental results with various encoder-decoder models are provided here:
 
-| Model                              | BLEU-1 | BLEU-2 | BLEU-3 | BLEU-4 |
-|-------------------------------------|--------|--------|--------|--------|
-| google/byt5-small                   | 0.3182 | 0.0963 | 0.0371 | 0.0040 |
-| Helsinki-NLP/opus-mt-ru-en          | 0.6588 | 0.4165 | 0.2697 | 0.1325 |
-| facebook/nllb-200-distilled-600M    | 0.8815 | 0.7640 | 0.6589 | 0.4434 |
-| facebook/mbart-large-50             | 0.9103 | 0.8395 | 0.7500 | 0.5179 |
-| facebook/m2m100_418M                | **0.9427** | **0.8891** | **0.8119** | **0.5819** |
+| Model                              | BLEU-1 | BLEU-2 | BLEU-3 | BLEU-4 | CHRF |
+|-------------------------------------|--------|--------|--------|--------|-------|
+| google/byt5-small                   | 0.3182 | 0.0963 | 0.0371 | 0.0040 | 31.19 |
+| Helsinki-NLP/opus-mt-ru-en          | 0.6588 | 0.4165 | 0.2697 | 0.1325 | 64.51 |
+| facebook/nllb-200-distilled-600M    | 0.8815 | 0.7640 | 0.6589 | 0.4434 | 85.21 |
+| facebook/mbart-large-50             | 0.9103 | 0.8395 | 0.7500 | 0.5179 | 89.21 |
+| facebook/m2m100_418M                | **0.9427** | **0.8891** | **0.8119** | **0.5819** | **92.72** |
--- src/metrics.py
+++ src/metrics.py
@@ -1,32 +1,49 @@
 from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
+from sacrebleu.metrics import CHRF
 from datasets import load_dataset
 
 # nltk.download("popular")
 
-dataset = load_dataset("Arseney/parallel_corpus_russian_rsl_glosses")
-target_data = [[item["rsl"].split()] for item in dataset["test"]]
-smooth = SmoothingFunction().method1
+def comet_metric_evaluation():
+    
+    
+def chrf_metric_evaluatuion():
+    dataset = load_dataset("Arseney/parallel_corpus_russian_rsl_glosses")
+    references = [[item["rsl"] for item in dataset["test"]]]
+    
+    with open("translated_sentences/opus.txt", "rt", encoding="utf-8") as file:
+        hypotheses = file.read().split("\n")
+    
+    metric = CHRF()
+    score = metric.corpus_score(hypotheses, references)
+    return score.score
 
-with open("translated_sentences\\nllb.txt", "rt", encoding="utf-8") as file:
-    hypotheses = [item.replace("\n", "").split() for item in file.readlines()]
+def bleu_metric_evaluation() -> dict:
+    dataset = load_dataset("Arseney/parallel_corpus_russian_rsl_glosses")
+    target_data = [[item["rsl"].split()] for item in dataset["test"]]
+    
+    smooth = SmoothingFunction().method1
 
-bleu_scores = {
-    "bleu_1": corpus_bleu(hypotheses=hypotheses, 
-                list_of_references=target_data, 
-                smoothing_function=smooth,
-                weights=(1, 0, 0, 0)),
-    "bleu_2": corpus_bleu(hypotheses=hypotheses, 
-                list_of_references=target_data, 
-                smoothing_function=smooth,
-                weights=(0, 1, 0, 0)),
-    "bleu_3": corpus_bleu(hypotheses=hypotheses, 
-                list_of_references=target_data, 
-                smoothing_function=smooth,
-                weights=(0, 0, 1, 0)),
-    "bleu_4": corpus_bleu(hypotheses=hypotheses, 
-                list_of_references=target_data, 
-                smoothing_function=smooth,
-                weights=(0, 0, 0, 1))
-}
+    with open("translated_sentences\\nllb.txt", "rt", encoding="utf-8") as file:
+        hypotheses = [item.replace("\n", "").split() for item in file.readlines()]
 
-print(bleu_scores)
+    bleu_scores = {
+        "bleu_1": corpus_bleu(hypotheses=hypotheses, 
+                    list_of_references=target_data, 
+                    smoothing_function=smooth,
+                    weights=(1, 0, 0, 0)),
+        "bleu_2": corpus_bleu(hypotheses=hypotheses, 
+                    list_of_references=target_data, 
+                    smoothing_function=smooth,
+                    weights=(0, 1, 0, 0)),
+        "bleu_3": corpus_bleu(hypotheses=hypotheses, 
+                    list_of_references=target_data, 
+                    smoothing_function=smooth,
+                    weights=(0, 0, 1, 0)),
+        "bleu_4": corpus_bleu(hypotheses=hypotheses, 
+                    list_of_references=target_data, 
+                    smoothing_function=smooth,
+                    weights=(0, 0, 0, 1))
+    }
+
+    return bleu_scores
